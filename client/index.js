d45ddea6c86a2f261d2346f8bd6ca96f4478a2ad
Chyld Medford <chyld.medford@gmail.com>
2015-04-10 14:15:16 -0700

    fin

--- client/index.js
+++ client/index.js
@@ -29,7 +29,49 @@ function move(){
   tgt.x = $target.data('x') * 1;
   tgt.y = $target.data('y') * 1;
 
-  console.log(src, tgt);
+  var compass = {};
+  compass.north = (current === 'js') ? -1 : 1;
+  compass.east = (current === 'js') ? 1 : -1;
+  compass.west = compass.east * -1;
+  compass.south = compass.north * -1;
+
+  switch(moveType(src, tgt, compass, isKing)){
+    case 'move':
+      console.log('move');
+      movePiece($source, $target);
+      switchUser();
+      break;
+    case 'jump':
+      console.log('jump');
+  }
+}
+
+function movePiece($source, $target){
+  var sourceClasses = $source.attr('class');
+  var targetClasses = $target.attr('class');
+
+  $target.attr('class', sourceClasses);
+  $source.attr('class', targetClasses);
+}
+
+function moveType(src, tgt, compass, isKing){
+  if(isMove(src, tgt, compass, isKing)){
+    return 'move';
+  }
+
+  if(isJump() && isEnemy()){
+    return 'jump';
+  }
+}
+
+function isMove(src, tgt, compass, isKing){
+  return (src.x + compass.east === tgt.x || src.x + compass.west === tgt.x) && (src.y + compass.north === tgt.y || (isKing && src.y + compass.south === tgt.y));
+}
+
+function isJump(){
+}
+
+function isEnemy(){
 }
 
 function select(){
